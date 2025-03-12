409bc837924e8a47f740d4d55c7d19e549da6fb8
wearrrrr <contact@wearr.dev>
2025-03-11 23:39:33 -0500

    make logging actually wait for \n to flush the buffer.

--- src/adb.ts
+++ src/adb.ts
@@ -82,17 +82,42 @@ export async function termuxCmdWait(cmd: string): Promise<AdbSubprocessWaitResul
   return await adb.subprocess.spawnAndWait(["run-as", "com.termux", "files/usr/bin/bash", "-c", `'export PATH=/data/data/com.termux/files/usr/bin:$PATH; export LD_PRELOAD=/data/data/com.termux/files/usr/lib/libtermux-exec.so; ${cmd}'`]);
 }
 
+let stdout_pending_data = "";
+let stderr_pending_data = "";
+
 function logProcess(process: AdbSubprocessProtocol) {
   process.stdout.pipeTo(new WritableStream({
     write(packet) {
-      console.log(new TextDecoder().decode(packet));
+      let data = new TextDecoder().decode(packet);
+      let lines = (stdout_pending_data + data).split("\n");
+      stdout_pending_data = lines.pop()!;
+      for (let line of lines) {
+        console.log(line);
+      }
     }
   }) as any);
+  
   process.stderr.pipeTo(new WritableStream({
     write(packet) {
-      console.error(new TextDecoder().decode(packet));
+      let data = new TextDecoder().decode(packet);
+      let lines = (stderr_pending_data + data).split("\n");
+      stderr_pending_data = lines.pop()!;
+      for (let line of lines) {
+        console.error(line);
+      }
     }
   }) as any);
+
+  // process.stdout.pipeTo(new WritableStream({
+  //   write(packet) {
+  //     console.log(new TextDecoder().decode(packet));
+  //   }
+  // }) as any);
+  // process.stderr.pipeTo(new WritableStream({
+  //   write(packet) {
+  //     console.error(new TextDecoder().decode(packet));
+  //   }
+  // }) as any);
 }
 
 export async function prootCmd(cmd: string): Promise<number> {
